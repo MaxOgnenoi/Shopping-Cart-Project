162ddb648a2a40a06d09c4d7455db3b8352c12f9
Max Ognenoi <maksymohnenyi>
2023-06-27 21:49:28 -0400

    some changes in server and app

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const routes = require('./routes/routes')
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const routes = require('./routes/routes');
 
-app.use(express.json())
-app.use(morgan('combined'))
-app.use('/routes', routes)
+app.use(express.json());
+app.use(morgan('combined'));
+app.use('/', routes);
 
-app.listen(3000, () => {
-    console.log('Running at 3000')
-})
-
-module.exports = app
+module.exports = app;
